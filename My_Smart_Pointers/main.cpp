8d3d0ce8510db0cf671b574ec987740f9e4e9659
Clara Fairbanks <clarafairbanks@claras-macbook-pro.local>
2018-09-18 09:02:20 -0400

    Initial Commit

---
+++ My_Smart_Pointers/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  My_Smart_Pointers
+//
+//  Created by Clara Fairbanks on 9/18/18.
+//  Copyright © 2018 Clara Fairbanks. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
